8f62a3ee8f700dc203dd8703c649aeec6980890b
Andrei <ierdna100@gmail.com>
2023-11-08 17:39:02 -0500

    Changed crash functionality

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ const express = require('express')
 
 require('dotenv').config()
 const app = express()
+/**@type boolean */
+let clientRunning = false
 
 /**@type Channel */
 let updateChannel
 let messageReference = undefined;
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 const logger = createLogger.default('Scheduling Bot')
 
 if (!process.env.TOKEN 
@@ -25,114 +26,134 @@ if (!process.env.TOKEN
     || !process.env.UPDATE_CHANNEL_ID)
 {
     logger.error(".env file is missing Token, client ID or channel ID. Fill those in!")
-    return
+    process.exit(1)
 }
 
-client.on('ready', () => {
-    updateChannel = client.channels.cache.get(process.env.UPDATE_CHANNEL_ID)
+createClient()
 
-    updateChannel.send(`<@337662083523018753> server started`)
+function createClient() {
+    const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-    logger.info(`Websocket ready and connected as: ${client.user.tag}`)
+    client.on('ready', () => {
+        updateChannel = client.channels.cache.get(process.env.UPDATE_CHANNEL_ID)
     
-    if (!process.env.UPDATE_RATE_SECONDS)
-    {
-        process.env.UPDATE_RATE_SECONDS = 120
-        logger.warn(`Update rate is not defined in .env file. Defaulting to 120 seconds (2 minutes)`)
-    }
-    else
-    {
-        logger.info(`Update rate is ${process.env.UPDATE_RATE_SECONDS} seconds.`)
-    }
-
-    UpdateSchedules()
+        //updateChannel.send(`<@337662083523018753> server started`)
     
-})
-
-client.on('interactionCreate', async interaction => {
-    if (interaction.isChatInputCommand())
-    {
-        let userID = interaction.user.id
-        let options = interaction.options
-
-        let isOp = IsUserOp(userID)
-        let isBanned = IsUserBanned(userID)
-
-        switch (interaction.commandName) {
-            case 'setdisplayname':
-                logger.info(`User <@${userID}> entered command 'setdisplayname'`)
-                await UpdateUserDisplayName(interaction, userID, options.get('name'))
-                break
-            case 'uploadschedule':
-                if (isBanned)
-                {
-                    logger.info(`User <@${userID}> cannot use command 'uploadschedule': banned`)
-                    await interaction.reply("**You canot upload a schedule: you are banned**")
-                    return
-                }
-
-                logger.info(`User <@${userID}> entered command 'uploadschedule'`)
-                await InitializeScheduleRequest(interaction)
-                break
-            case 'clearschedule':
-                logger.info(`User <@${userID}> entered command 'clearschedule'`)
-                await ClearUserSchedule(interaction, userID)
-                break
-            case 'getschedule':
-                logger.info(`User <@${userID}> entered command 'getschedule'`)
-                await GetScheduleFormatted(interaction, userID, options.get('user'))
-                break
-            case 'getschedulejson':
-                logger.info(`User <@${userID}> entered command 'getschedulejson'`)
-                await ReplyWithSchedule(interaction, userID, options.get('user'))
-                break
-            case 'help':
-                logger.info(`User <@${userID}> entered command 'help'`)
-                await interaction.reply({ files: [ "./images/uploadingScheduleInfoV2.png" ]})
-                break
-            case 'getusers':
-                logger.info(`User <@${userID}> entered command 'getusers'`)
-                await ReplyWithLoggedUsers(interaction)
-                break
-            case 'removeschedule':
-                if (!isOp)
-                {
-                    logger.info(`User <@${userID}> cannot use command 'removeschedule': not admin`)
-                    await interaction.reply("**You cannot remove user from board: you are not an admin**")
-                    return
-                }
-
-                logger.info(`User <@${userID}> entered command 'removeschedule'`)
-                await RemoveLoggedUser(interaction, userID, options.get('user'))
-                break
-            case 'banlist':
-                if (!isOp)
-                {
-                    logger.info(`User <@${userID}> cannot use command 'removeuser': not admin`)
-                    await interaction.reply("**You cannot modify banlist: you are not an admin**")
-                    return
-                }
-
-                logger.info(`User <@${userID}> entered command 'removeuser'`)
-                await BanlistHandler(interaction, userID, options.getSubcommand(), options.get('user'))
-                break
-            default:
-                logger.warn(`Command ${interaction.commandName} is not handled!`)
-                await interaction.reply({ content: "`501 - Not Implemented`" })
-        }
-    }
-    else if (interaction.isModalSubmit())
-    {
-        let userID = interaction.user.id
-        let components = interaction.components
-
-        if (interaction.customId == "schedule_modal")
+        logger.info(`Websocket ready and connected as: ${client.user.tag}`)
+        
+        if (!process.env.UPDATE_RATE_SECONDS)
         {
-            logger.info(`User <@${userID}> submitted modal 'schedule_modal'`)
-            UploadUserSchedule(interaction, userID, components)
+            process.env.UPDATE_RATE_SECONDS = 120
+            logger.warn(`Update rate is not defined in .env file. Defaulting to 120 seconds (2 minutes)`)
         }
-    }
-})
+        else
+        {
+            logger.info(`Update rate is ${process.env.UPDATE_RATE_SECONDS} seconds.`)
+        }
+    
+        UpdateSchedules()
+        clientRunning = true
+    })
+    
+    client.on('interactionCreate', async interaction => {
+        if (interaction.isChatInputCommand())
+        {
+            let userID = interaction.user.id
+            let options = interaction.options
+    
+            let isOp = IsUserOp(userID)
+            let isBanned = IsUserBanned(userID)
+    
+            switch (interaction.commandName) {
+                case 'setdisplayname':
+                    logger.info(`User <@${userID}> entered command 'setdisplayname'`)
+                    await UpdateUserDisplayName(interaction, userID, options.get('name'))
+                    break
+                case 'uploadschedule':
+                    if (isBanned)
+                    {
+                        logger.info(`User <@${userID}> cannot use command 'uploadschedule': banned`)
+                        await interaction.reply("**You canot upload a schedule: you are banned**")
+                        return
+                    }
+    
+                    logger.info(`User <@${userID}> entered command 'uploadschedule'`)
+                    await InitializeScheduleRequest(interaction)
+                    break
+                case 'clearschedule':
+                    logger.info(`User <@${userID}> entered command 'clearschedule'`)
+                    await ClearUserSchedule(interaction, userID)
+                    break
+                case 'getschedule':
+                    logger.info(`User <@${userID}> entered command 'getschedule'`)
+                    await GetScheduleFormatted(interaction, userID, options.get('user'))
+                    break
+                case 'getschedulejson':
+                    logger.info(`User <@${userID}> entered command 'getschedulejson'`)
+                    await ReplyWithSchedule(interaction, userID, options.get('user'))
+                    break
+                case 'help':
+                    logger.info(`User <@${userID}> entered command 'help'`)
+                    await interaction.reply({ files: [ "./images/uploadingScheduleInfoV2.png" ]})
+                    break
+                case 'getusers':
+                    logger.info(`User <@${userID}> entered command 'getusers'`)
+                    await ReplyWithLoggedUsers(interaction)
+                    break
+                case 'removeschedule':
+                    if (!isOp)
+                    {
+                        logger.info(`User <@${userID}> cannot use command 'removeschedule': not admin`)
+                        await interaction.reply("**You cannot remove user from board: you are not an admin**")
+                        return
+                    }
+    
+                    logger.info(`User <@${userID}> entered command 'removeschedule'`)
+                    await RemoveLoggedUser(interaction, userID, options.get('user'))
+                    break
+                case 'banlist':
+                    if (!isOp)
+                    {
+                        logger.info(`User <@${userID}> cannot use command 'removeuser': not admin`)
+                        await interaction.reply("**You cannot modify banlist: you are not an admin**")
+                        return
+                    }
+    
+                    logger.info(`User <@${userID}> entered command 'removeuser'`)
+                    await BanlistHandler(interaction, userID, options.getSubcommand(), options.get('user'))
+                    break
+                default:
+                    logger.warn(`Command ${interaction.commandName} is not handled!`)
+                    await interaction.reply({ content: "`501 - Not Implemented`" })
+            }
+        }
+        else if (interaction.isModalSubmit())
+        {
+            let userID = interaction.user.id
+            let components = interaction.components
+    
+            if (interaction.customId == "schedule_modal")
+            {
+                logger.info(`User <@${userID}> submitted modal 'schedule_modal'`)
+                UploadUserSchedule(interaction, userID, components)
+            }
+        }
+    })
+    
+    client.on('error', error => {
+        clientRunning = false
+        client.destroy()
+
+        logger.error(`Server crashed with error name: ${error.name}`)
+        logger.error(`${error.message}`)
+        logger.error("Attempting restart in 10 minutes!")
+
+        setTimeout(createClient, 10 * 60 * 1000)
+    })
+
+    logger.info("Attempting to connect to Discord with websocket...")
+    client.login(process.env.TOKEN)
+}
 
 async function UpdateSchedules() {
     //If message presumably doesn't exist
@@ -178,11 +199,9 @@ function ResetMessageIDInfo()
 }
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send(`Server running: ${clientRunning}`)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.DEBUG_PORT, () => {
+    console.log(`Debugging listening on port: ${process.env.DEBUG_PORT}`)
 })
-
-client.login(process.env.TOKEN)
