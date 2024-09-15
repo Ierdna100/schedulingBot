f8427f41e6665f59f7cad115d264c85587cca674
Andrei <ierdna100@yahoo.com>
2024-09-15 12:55:48 -0400

    New better installation system

--- src/Install.ts
+++ src/Install.ts
@@ -1,11 +1,44 @@
 import fs from "fs";
-import { EnvManager } from "./env/EnvManager.js";
+import { EnvFileFields } from "./env/EnvManager.js";
+import { ANSI } from "./dto/ANSIColors.js";
 
 async function install() {
-    fs.writeFileSync("./.env", EnvManager.generateTemplate());
-    fs.mkdirSync("./logs/");
-    fs.mkdirSync("./static/");
-    fs.mkdirSync("./static/instructionsImages");
+    console.log(ANSI.blue + "Setting up project...");
+    onlyAddNewFieldsOrCreate("./env.json", new EnvFileFields());
+    checkExistsAndMkdir("./logs/");
+    checkExistsAndMkdir("./static/");
+    checkExistsAndMkdir("./static/instructionsImages");
+    console.log(ANSI.green);
+    console.log("Successfully setup project!" + ANSI.clear);
+}
+
+function onlyAddNewFieldsOrCreate(filepath: string, data: any, prettyJson = true) {
+    let writeData = data;
+
+    if (fs.existsSync(filepath)) {
+        console.log("Appending fields to env.json, as it alread exists...");
+        const existingData = JSON.parse(fs.readFileSync(filepath).toString());
+        writeData = { ...data, ...existingData };
+    }
+
+    console.log("Writing to environnement file...");
+    if (prettyJson) {
+        fs.writeFileSync(filepath, JSON.stringify(writeData, null, "\t"));
+    } else {
+        fs.writeFileSync(filepath, JSON.stringify(writeData));
+    }
+    console.log("Successfully written to environnement file!");
+}
+
+function checkExistsAndMkdir(filepath: string) {
+    if (fs.existsSync(filepath)) {
+        console.log(`Directory to initialize '${filepath}' already exists, skipping...`);
+        return;
+    }
+
+    console.log(`Creating directory '${filepath}'...`);
+    fs.mkdirSync(filepath);
+    console.log("Successfully created directory!");
 }
 
 install();
